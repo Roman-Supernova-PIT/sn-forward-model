7b1dae641d0a3aa8945c0ac1c58ba9e0fa2a67e9
Michael Wood-Vasey <wmwv@pitt.edu>
2024-07-17 16:48:40 -0400

    Fix up INFODIR for test_one_lightcurve.sh

--- tests/test_one_lightcurve.sh
+++ tests/test_one_lightcurve.sh
@@ -4,7 +4,6 @@
 # Update the variables in the script to match your local system locations for DATADIR.
 # In DATADIR should be the subdirectories 'images' and 'truth'.
 DATADIR=${HOME}/data/RomanDESCSims
-
 # In INFODIR should be transient_info_table.csv
 #                      transient_host_info_table.csv
 # We have these in tests/data for a few transients, so we can use those to test
