39b667310f44dbb77c9912221107de3d1d64e573
Michael Wood-Vasey <wmwv@pitt.edu>
2024-07-17 14:54:33 -0400

    Make test_one_lightcurve.sh work with infodir, datadir.

--- tests/test_one_lightcurve.sh
+++ tests/test_one_lightcurve.sh
@@ -1,2 +1,5 @@
+# Change this to what the RomanDESCSims data are.
+# Below DATADIR should be the subdirectories 'images' and 'truth'.
+DATADIR=${HOME}/data/RomanDESCSims
 # Run from base directoroy
-python python/RomanDESCForwardModelLightcurves.py 41024123441
+python python/RomanDESCForwardModelLightcurves.py 41024123441 --infodir tests/data/RomanDesc --datadir ${DATADIR} --dataset RomanDESC
