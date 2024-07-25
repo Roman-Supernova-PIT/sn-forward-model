e2d501c645d9f4e732fb60254224b8d2ba9f2038
Michael Wood-Vasey <wmwv@pitt.edu>
2024-07-25 11:11:14 -0700

    Get list and table.

--- python/find_rubin_images_for_ra_dec.py
+++ python/find_rubin_images_for_ra_dec.py
@@ -1,5 +1,14 @@
-from lsst.daf.butler import Butler
+"""
+Query RomanDESCSims Rubin data processing on NERSC. 
+Requires LSST Science Pipelines to query images and get data Ids
+Goal to get filepaths to feed to rest of forward-modeling package
+which doesn not require Sicence Pipelines.
+"""
+from lsst.daf.butler import Butler, Timespan
 from lsst import sphgeom
+
+import astropy.time
+from astropy.table import Table
 
 transient_id = 30328322
 ra, dec = 8.52941151,-43.0266337
@@ -22,5 +31,42 @@ htm_id = pixelization.index(
         sphgeom.LonLat.fromDegrees(ra, dec)
     )
 )
+start_time = astropy.time.Time(mjd_start, format="mjd")
+end_time = astropy.time.Time(mjd_end, format="mjd")
 
-dataset_refs = butler.registry.queryDatasets("calexp", htm20=htm_id)
+before_timespan = Timespan(None, start_time)
+during_timespan = Timespan(start_time, end_time)
+after_timespan = Timespan(end_time, None)
+
+
+before_dataset_refs = butler.registry.queryDatasets("calexp", htm20=htm_id,
+                                                    band="r",
+                                     where="visit.timespan OVERLAPS my_timespan",
+                                     bind={"my_timespan": before_timespan})
+during_dataset_refs = butler.registry.queryDatasets("calexp", htm20=htm_id,
+                                                    band="r",
+                                     where="visit.timespan OVERLAPS my_timespan",
+                                     bind={"my_timespan": during_timespan})
+after_dataset_refs = butler.registry.queryDatasets("calexp", htm20=htm_id,
+                                                    band="r",
+                                     where="visit.timespan OVERLAPS my_timespan",
+                                     bind={"my_timespan": after_timespan})
+
+# Realize into list
+before_dataset_refs = list(before_dataset_refs)
+during_dataset_refs = list(during_dataset_refs)
+after_dataset_refs = list(after_dataset_refs)
+
+# Extract visit, band, detector
+rows = \
+[(transient_id, dr.dataId['instrument'], dr.dataId['visit'], dr.dataId['band'], dr.dataId['detector']) for dr in during_dataset_refs]
+ddr_table = Table(rows=rows, names=("transient_id", "instrument", "visit", "band", "detector"))
+
+before_dataset_refs[
+[f"{transient_id} {dr.dataId['instrument']} {dr.dataId['visit']} {dr.dataId['band']} {dr.dataId['detector']}" for dr in during_dataset_refs]
+before_dataset_refs[
+
+# Get URL (On NERSC these are filepaths)
+bdr = [butler.getURI(dr).geturl() for dr in before_dataset_refs]
+ddr = [butler.getURI(dr).geturl() for dr in during_dataset_refs]
+adr = [butler.getURI(dr).geturl() for dr in after_dataset_refs]
